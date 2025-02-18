91b2311aa49849115f9df01ce2fd437b99846791
Noey <ignacio.francis@aelluminate.com>
2025-02-19 04:38:57 +0800

    feat: add user context

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { metadata as SiteData } from "@/lib/metadata"
 export const metadata: Metadata = SiteData
 
 import { ThemeProvider } from "@/components/provider/theme-provider"
+import { UserProvider } from "@/components/provider/user-provider"
 
 export default function RootLayout({
   children,
@@ -16,9 +17,16 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={`${geist.className}`}>
       <body className="antialiased">
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          {children}
-        </ThemeProvider>
+        <UserProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </UserProvider>
       </body>
     </html>
   )
