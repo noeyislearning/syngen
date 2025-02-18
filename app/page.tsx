8f1d3f0abcec5a0953fe6fb8006f88f4e419871f
Noey <ignacio.francis@aelluminate.com>
2025-02-19 03:58:01 +0800

    refactor: rename LoginPage to AuthPage for better clarity

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { AuthForm } from "@/components/ui/auth-form"
 
-export default function LoginPage() {
+export default function AuthPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-inherit p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col items-center gap-6">
